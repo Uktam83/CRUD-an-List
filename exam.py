452723a5bbe129f04afe815b9130cf9c2c761ebc
Uktam83 <usanayev83@gmail.com>
2024-12-16 18:15:29 +0500

    solve

---
+++ exam.py
@@ -0,0 +1,9 @@
+royxat_mevalar = [] # create
+for x in range(5): 
+    a = input() #read
+    royxat_mevalar.append(a)
+print(royxat_mevalar) #list of elements
+royxat_mevalar [2] = "kiwi" #update
+print(royxat_mevalar) #list of elements update
+royxat_mevalar.clear() #delete
+print(royxat_mevalar) #[]
